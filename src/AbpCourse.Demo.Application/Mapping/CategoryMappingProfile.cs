0569aa95224124ca80173d7c51f80d2362aaab8d
YaraGh <yaraesmaiel123@gmail.com>
2024-09-25 07:58:53 -0500

    Mapping

---
+++ src/AbpCourse.Demo.Application/Mapping/CategoryMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AbpCourse.Demo.Categories;
+using AbpCourse.Demo.Entities.Categories;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbpCourse.Demo.Mapping
+{
+    public class CategoryMappingProfile : Profile // profile is auto Mapper
+    {
+        public CategoryMappingProfile()
+        {
+            CreateMap<Category,CategoryDto>();
+            CreateMap<CreateUpdateCategoryDto, Category>();
+        }
+    }
+}
